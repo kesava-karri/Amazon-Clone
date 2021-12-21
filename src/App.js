a6f520912fa93ccd24a8b96dff363e4fd8294a23
Kesava Karri <kkeshav1998@gmail.com>
2021-12-20 23:41:17 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Html } from "@mui/icons-material";
 import React from "react";
 import "./App.css";
 import Header from "./Header.js"; 
 import Home from "./Home"; // We can either include js extension or leave it as such which means .js
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   // App component
   return (
-    <div className="app"> {/* Good to use lowercase classNames */}
-      {/* Header */}
+    <Router>
+      <div className="app"> {/* Good to use lowercase classNames */}
       <Header />
-      {/* Home */}
-      <Home />
-    </div>
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Checkout.css";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+			<div className="checkout__left">
+				<img 
+					className="checkout__ad"
+					src="https://m.media-amazon.com/images/I/716kWBue3fL._SX3000_.jpg"
+					alt="ad"
+				/>
+			</div>
+    </div>
+  );
+}
+
+export default Checkout;
